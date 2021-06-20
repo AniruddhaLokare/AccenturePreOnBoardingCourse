385be171341739aad31a6eaa745da3bd338e7510
AniruddhaLokare <58508337+AniruddhaLokare@users.noreply.github.com>
2021-06-20 21:14:08 +0530

    Unix

---
+++ Unix/Introduction to Unix.sh
@@ -0,0 +1,31 @@
+#Display the previous, current and next month calendar of the  year 1987 august
+cal 08 1987 -3
+
+#Display and see what is the specialty of September 1752.
+cal 09 1752
+
+#Write a command to display the previous, current and next month calendar of the year 2015 December.
+cal 12 2015 -3
+
+#Write a command to display the current day as full weekday name.(e.g., Wednesday)
+date "+%A"
+
+#Write a command to display AM or PM in lower case.
+date "+%P"
+
+#What is the command to perform all the arithmetic operations using basic calculator.
+#Note: Specify only the command.
+bc
+
+#What is the option in ls command to display the hidden files or directories?
+ls -a 
+
+
+#What is the command to list the contents of the current directory in comma separated fashion
+ls -m
+
+#What is the command to display the disk usage of the given directory Contest.
+du Contest
+
+#What is the command to display the disk space usage the current directory excluding the size of subdirectories
+du -Sd 1
